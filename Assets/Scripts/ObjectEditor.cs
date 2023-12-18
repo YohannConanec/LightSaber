6dc5ce54c35e71bbfbe04bce15956af2a13f30c6
Yohann CONANEC <yconanec@etud.univ-angers.fr>
2023-12-18 09:50:46 +0100

    reparation editor

--- Assets/Scripts/ObjectEditor.cs
+++ Assets/Scripts/ObjectEditor.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[CustomEditor(typeof(AddObjects))]
-public class ObjectEditor //: Editor
+[CustomEditor(typeof(AddObjects))]
+public class ObjectEditor : Editor
 {
-    /*public override void OnInspectorGUI()
+    public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        AddObjects func = (AddObjects)target;
+        AddObjects ao = (AddObjects)target;
 
-        if (GUILayout.Button("Add Object"))
+        if (GUILayout.Button("Add Object L")) //to click on play mode
         {
-            func.InstanciateObject();
+            ao.InstanciateObject(true);
         }
-    }*/
+        
+        if (GUILayout.Button("Add Object R")) //to click on play mode
+        {
+            ao.InstanciateObject(false);
+        }
+
+        if (GUILayout.Button("Clear JSON")) //to click off play mode
+        {
+            ao.ClearJSON();
+        }
+
+        if (GUILayout.Button("INSERT OBJECTS")) //to click off play mode
+        {
+            ao.InsertObjects();
+        }
+
+        
+
+
+    }
     
 }
