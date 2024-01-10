5c7843c82bbfd59cc372bffe31c1c91d0437f02a
Yohann CONANEC <yconanec@etud.univ-angers.fr>
2024-01-10 14:26:40 +0100

    pause son

--- Assets/Scripts/MenuPauseManager.cs
+++ Assets/Scripts/MenuPauseManager.cs
@@ -7,6 +7,7 @@ public class MenuPauseManager : MonoBehaviour
     public GameObject buzzer; 
     public GameObject menuPause;
     public GameObject song1;
+    public GameObject songClip;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +23,14 @@ public class MenuPauseManager : MonoBehaviour
         buzzer.SetActive(false);
         menuPause.SetActive(true);
         song1.GetComponent<LevelManager>().SetSpeed(0);
+        songClip.GetComponent<AudioSource>().Pause();
 
     }
     public void PauseOff(){
         buzzer.SetActive(true);
         menuPause.SetActive(false);
         song1.GetComponent<LevelManager>().SetSpeed(4);
+        songClip.GetComponent<AudioSource>().Play();
     }
     
 }
